37cec6e849e23dbda867246154090abc06edefb4
ziaurrehmanrehmani <ziaurrehmanrehmani@gmail.com>
2022-04-09 13:09:41 -0700

    First Commit no error justt 1 warnning

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Vagie from "../components/Vagie";
+import Popular from "../components/Popular";
+
+
+
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Vagie/>
+        <Popular/>
+       
+
+
+    </div>
+  )
+}
+
+export default Home
